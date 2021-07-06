0758ec5415489bea62d20db00d24ef4bc074f8ac
wjsxqs <xsmlcp@163.com>
2021-07-06 13:00:44 +0800

    Update build

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 rustup target add wasm32-unknown-unknown
+
 RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
-cd ..
+mkdir -p res
 cp target/wasm32-unknown-unknown/release/nft.wasm ./res/nft.wasm
